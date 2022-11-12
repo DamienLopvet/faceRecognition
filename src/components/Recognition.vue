da5613d8d551e8863f431729066eaf59e29ff6bf
damien lopvet <damienlopvet@gmail.co>
2022-11-12 10:52:31 +0100

    add emoji

--- src/components/Recognition.vue
+++ src/components/Recognition.vue
@@ -1,4 +1,5 @@
 <template>
+    <h1>{{ emoji }}</h1>
     <div class="container">
         <video
             src=""
@@ -15,20 +16,26 @@
     import * as faceapi from "face-api.js";
     export default {
         name: "HelloWorld",
-
+        data() {
+            return {
+                expression: "",
+                emoji:""
+            };
+        },
         methods: {
             loadModules() {
                 faceapi.nets.tinyFaceDetector.loadFromUri("./weights");
                 faceapi.nets.faceLandmark68Net.loadFromUri("./weights");
                 faceapi.nets.faceRecognitionNet.loadFromUri("./weights");
                 faceapi.nets.faceExpressionNet.loadFromUri("./weights");
-                
+                faceapi.nets.ageGenderNet.loadFromUri("./weights");
             },
             startVideo() {
-              navigator.getUserMedia = ( navigator.getUserMedia ||
-                       navigator.webkitGetUserMedia ||
-                       navigator.mozGetUserMedia ||
-                       navigator.msGetUserMedia);
+                navigator.getUserMedia =
+                    navigator.getUserMedia ||
+                    navigator.webkitGetUserMedia ||
+                    navigator.mozGetUserMedia ||
+                    navigator.msGetUserMedia;
                 const video = document.querySelector("video");
                 navigator.getUserMedia(
                     {
@@ -38,42 +45,109 @@
                     (err) => console.error(err)
                 );
             },
+            getExpression(e) {
+                let sortable = [];
+                for (var expression in e) {
+                    sortable.push([expression, e[expression]]);
+                }
+                sortable.sort(function (a, b) {
+                    return b[1] - a[1];
+                });
+                this.expression = sortable[0][0];
+            },
             faceDetection() {
                 const video = document.querySelector("video");
-                const container = document.querySelector(".container")
-                const canvas = faceapi.createCanvasFromMedia(video)
-                console.log(canvas);
-                container.append(canvas)
-                const displaySize = {width: video.width,height:video.height}
-                faceapi.matchDimensions(canvas, displaySize)
+                const container = document.querySelector(".container");
+                const canvas = faceapi.createCanvasFromMedia(video);
+                container.append(canvas);
+                const displaySize = {
+                    width: video.width,
+                    height: video.height,
+                };
+                faceapi.matchDimensions(canvas, displaySize);
                 setInterval(async () => {
-                    const detections =await faceapi
+                    const detections = await faceapi
                         .detectAllFaces(
                             video,
                             new faceapi.TinyFaceDetectorOptions()
                         )
                         .withFaceLandmarks()
-                        .withFaceExpressions();
-                        const resizedDetections = faceapi.resizeResults(detections, displaySize);
-                        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-                    faceapi.draw.drawDetections(canvas, resizedDetections)
-                    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-                    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-                  
-                }, 1000);
+                        .withFaceExpressions()
+                        .withAgeAndGender();
+                    console.log(detections);
+                    this.getExpression(detections[0].expressions);
+
+                    const resizedDetections = faceapi.resizeResults(
+                        detections,
+                        displaySize
+                    );
+                    canvas
+                        .getContext("2d")
+                        .clearRect(0, 0, canvas.width, canvas.height);
+                    faceapi.draw.drawDetections(canvas, resizedDetections);
+                    faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+                    resizedDetections.forEach((detection) => {
+                        const box = detection.detection.box;
+                        const drawBox = new faceapi.draw.DrawBox(box, {
+                            label:
+                                detection.gender +
+                                ", environ " +
+                                Math.round(detection.age) +
+                                " ans ",
+                        });
+                        drawBox.draw(canvas);
+                    });
+                }, 4000);
+            },
+        },
+        watch: {
+            expression: function (val) {
+                console.log(val);
+                switch (val) {
+                    case "angry":
+                        this.emoji = "😠";
+                        break;
+                    case "disgusted":
+                        this.emoji = "😖";
+                        break;
+                    case "fearful":
+                        this.emoji = "😨";
+                        break;
+                    case "happy":
+                        this.emoji = "😃";
+                        break;
+                    case "neutral":
+                        this.emoji = "😐";
+                        break;
+                    case "sad":
+                        this.emoji = "🥺";
+                        break;
+                    case "surprised":
+                        this.emoji = "😮";
+                        break;
+
+                    default:
+                        this.emoji = "😐"
+                        break;
+                }
             },
         },
         mounted() {
-          this.startVideo();
+            this.startVideo();
         },
-        beforeMount(){
-          this.loadModules();
-        }
+        beforeMount() {
+            this.loadModules();
+        },
     };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
+    h1 {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+    }
     .container {
         margin: 0;
         padding: 0;
